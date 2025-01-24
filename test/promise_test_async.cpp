bee6c3bbd34fa32ae3e4b154f509d383834b7b45
ivo <ivo.ivanov@hs-augsburg.de>
2025-01-24 16:19:23 +0100

    co_await compiles

--- test/promise_test_async.cpp
+++ test/promise_test_async.cpp
@@ -23,14 +23,19 @@ struct AwaitablePromise  {
   using promise_type = Self;
     Self get_return_object() { 
       //auto child = Self::create_from_impl(observable_->then(f));
-        std::cout << "get_return_object  " << std::endl;
+        std::cout << "get_return_object called, this is " << std::hex << size_t(this) << std::endl;
         return *this; 
     }
     std::suspend_never initial_suspend() { 
         std::cout << "initial_suspend  " << std::endl;
         return {}; 
     }
-    void return_void() {}
+    /// TODO enable if Value == Nothing
+    //void return_void() {}
+    /// TODO enable if Value != Nothing
+    Value return_value() {
+      return get_promise()->value();
+    }
     void unhandled_exception() {}
     std::suspend_always final_suspend() noexcept { 
         std::cout << "final_suspend  " << std::endl;
@@ -38,7 +43,9 @@ struct AwaitablePromise  {
     }
     
 
-  
+    AwaitablePromise() {
+      std::cout << "Ctor called , this is " << std::hex << size_t(this) << std::endl;
+    }
   /// Pattern-maching factory function that creates a New Self with different value and error types
   /// based on the passed implementation pointer. 
   template <class NewVal, class NewErr>
@@ -50,7 +57,7 @@ struct AwaitablePromise  {
     return new_obs;
   }
   std::shared_ptr<Impl> observable_{impl::create_observable<Impl>()};
-
+  std::shared_ptr<Impl> get_promise() const {return this->observable_;}
 
 
   [[nodiscard]] auto operator co_await() const noexcept {
@@ -59,7 +66,7 @@ struct AwaitablePromise  {
 				[[nodiscard]] bool await_ready() noexcept {
 					return m_state->has_value();
 				}
-				[[nodiscard]] bool await_suspend() noexcept {
+				[[nodiscard]] bool await_suspend(std::coroutine_handle<AwaitablePromise> handle) noexcept {
 					
 					
 					if (m_state->has_value()) {
@@ -99,7 +106,8 @@ using APromise = icey::AwaitablePromise<ResolveValue, ErrorValue>;
 
 APromise test_async_await() {
   std::cout << "test_async_await  " << std::endl;
-  co_return;
+  //co_return;
+  co_await APromise {};
 }
 
 int main(int argc, char **argv) {
