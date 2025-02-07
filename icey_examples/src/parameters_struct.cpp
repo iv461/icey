154d86a7076c6da938ab10be4c14afe5e868acef
ivo <ivo.ivanov@hs-augsburg.de>
2025-02-07 17:37:35 +0100

    Unsigned param

--- icey_examples/src/parameters_struct.cpp
+++ icey_examples/src/parameters_struct.cpp
@@ -3,10 +3,12 @@
 
 /// Here you declare in a single struct all parameters of the node:
 struct NodeParameters {
-  /// We can have regular fields 
+  /// We can have regular fields :
   double amplitude{3};
+  /// And also unsigned ones:
+  uint32_t number_of_attempts{3};
 
-  /// And as well parameters with constraints and a description
+  /// And as well parameters with constraints and a description:
   icey::Parameter<double> frequency{10., icey::Interval(0., 25.),
                                        std::string("The frequency of the sine")};
   
@@ -16,11 +18,10 @@ struct NodeParameters {
     double max_amp = 6.;
     std::vector<double> cov;
   } others;
-
 };
 
 int main(int argc, char **argv) {  
-  auto node = icey::create_node<icey::Node>(argc, argv, "parameters_struct");
+  auto node = icey::create_node<icey::Node>(argc, argv, "parameters_struct_example");
   
   /// Now create an object of the node-parameters that will be updated:
   NodeParameters params;
