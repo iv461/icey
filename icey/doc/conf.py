6f30fd90b43037ea65056b2795a0482c39fb4791
ivo <ivo.ivanov@hs-augsburg.de>
2025-02-11 23:57:18 +0100

    Added better css

--- icey/doc/conf.py
+++ icey/doc/conf.py
@@ -1,26 +1,16 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import subprocess, os
 
 project = 'icey'
 copyright = '2025, Ivo Ivanov'
 author = 'Ivo Ivanov'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "breathe",
     "myst_parser",
     "sphinx_copybutton",
+    "sphinxcontrib.spelling",
 ]
 
 breathe_projects = {"icey": ".build/doxygenxml/"}
@@ -31,21 +21,11 @@ breathe_default_members = ('members', 'undoc-members')
 templates_path = ['_templates']
 exclude_patterns = []
 
-import subprocess, os
 
-# Doxygen
-subprocess.call('doxygen Doxyfile', shell=True)
+#subprocess.call('doxygen Doxyfile', shell=True)
 
 highlight_language = 'c++'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'furo'
 html_static_path = ['_static']
-html_theme_options = {
-    "dark_css_variables": {
-        "color-brand-primary": "#7C4DFF",
-        "color-brand-content": "#7C4DFF",
-    },
-}
+html_css_files = ["icey.css"]
