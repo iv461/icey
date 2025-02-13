9358334bd6fee6e69e24b9f97698355ff0d12eed
ivo <ivo.ivanov@hs-augsburg.de>
2025-02-13 18:59:46 +0100

    Small doc fix

--- icey/include/icey/icey.hpp
+++ icey/include/icey/icey.hpp
@@ -93,8 +93,8 @@ struct NodeInterfaces {
 /// systems. It is implemented similarly to the tf2_ros::TransformListener. 
 /// Every time a new message is receved on /tf, it checks whether a relevant transforms (i.e. ones we subscribed) was received.
 /// It is therefore an asynchronous interface to TF, similar to the tf2_ros::AsynchBuffer. But the key difference is that 
-/// tf2_ros::AsynchBuffer can only deliver the transform once -- it is a promise, not a stream. 
-/// We want however to receive a continous stream of transforms, like a subscriber. This TFListener is used to implement the TransformSubscriptionStream.
+/// tf2_ros::AsynchBuffer can only deliver the transform once, it is therefore a promise, not a stream. 
+/// We want however to receive a continous stream of transforms, like a subscriber. This class is used to implement the TransformSubscriptionStream.
 /// \todo We could speed up the code a bit here (not sure if significantly), for this we could obtain the
 /// path in the TF tree between the source- and target-frame using tf2::BufferCore::_chainAsVector
 /// and then only react if any transform was received that is part of this path.
