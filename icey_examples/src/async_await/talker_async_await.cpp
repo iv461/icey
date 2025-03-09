cc71b644f7daeb97e7e27e2e2d054f43cc6b79d4
ivo <ivo.ivanov@hs-augsburg.de>
2025-03-09 01:09:01 +0100

    Nope, output Future dtor is still called

--- icey_examples/src/async_await/talker_async_await.cpp
+++ icey_examples/src/async_await/talker_async_await.cpp
@@ -15,14 +15,15 @@ icey::Future<std_msgs::msg::String> async_create_message(auto &timer) {
     co_return message;
 }
 
-icey::Stream<int> talk(std::shared_ptr<icey::Node> node) {    
-    auto timer = node->icey().create_timer(100ms);
-    auto pub = node->icey().create_publisher<std_msgs::msg::String>("/strings");
-
+icey::Stream<int> talk(std::shared_ptr<icey::Node> &node,
+    icey::TimerStream &timer, auto &pub) {    
+    
+        
     
     while(true) {
         std::cout << "3. B4 await " << std::endl;
-        std_msgs::msg::String message = co_await async_create_message(timer);
+        auto ret2 = async_create_message(timer);
+        std_msgs::msg::String message = co_await ret2;
         std::cout << "4. After await async_create_message " << std::endl;
         RCLCPP_INFO_STREAM(node->get_logger(), "Publishing: " << message.data);
         pub.publish(message);
@@ -32,9 +33,11 @@ icey::Stream<int> talk(std::shared_ptr<icey::Node> node) {
 
 
 int main(int argc, char **argv) {
-  icey::icey_coro_debug_print = true;
-  auto node = icey::create_node(argc, argv, "talker_node");
-  talk(node);
+    icey::icey_coro_debug_print = true;
+auto node = icey::create_node(argc, argv, "talker_node");
+auto timer = node->icey().create_timer(100ms);
+auto pub = node->icey().create_publisher<std_msgs::msg::String>("/strings");
+  talk(node, timer, pub);
   std::cout << "5. After talks " << std::endl;
   icey::spin(node);
 }
