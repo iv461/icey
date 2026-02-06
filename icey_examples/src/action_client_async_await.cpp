318187e5fe4e62e37794e767e2240cebd8d0f57c
Ivo Ivanov <ivo.ivanov@tha.de>
2026-02-06 12:58:04 +0100

    Updated action examples

--- icey_examples/src/action_client_async_await.cpp
+++ icey_examples/src/action_client_async_await.cpp
@@ -16,36 +16,33 @@
 using namespace std::chrono_literals;
 using Fibonacci = example_interfaces::action::Fibonacci;
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
+icey::Promise<void> call_action() {
   auto node = std::make_shared<rclcpp::Node>("icey_action_client_async_await_example");
   auto ctx = std::make_shared<icey::ContextAsyncAwait>(node.get());
+  auto client = ctx->create_action_client<Fibonacci>("/icey_test_action_fibonacci");
 
-  // Create ICEY action client
-  auto client = ctx->create_action_client<Fibonacci>("/fibonacci");
+  Fibonacci::Goal goal;
+  goal.order = 7;
 
-  // Periodically send a goal and co_await result
-  ctx->create_timer_async(1s, [node, client](std::size_t) -> icey::Promise<void> {
-    Fibonacci::Goal goal;
-    goal.order = 7;
+  RCLCPP_INFO_STREAM(node->get_logger(), "Sending goal.. ");
+  // Request a goal with 2 seconds timeout
+  auto maybe_goal_handle = co_await client.send_goal(
+      goal, 2s, [node](auto, auto) { RCLCPP_WARN(node->get_logger(), "Got some feedback"); });
 
-    // Request a goal with 2 seconds timeout
-    auto maybe_goal_handle = co_await client.send_goal(
-        goal, 2s, [node](auto, auto) { RCLCPP_WARN(node->get_logger(), "Got some feedback"); });
+  if (maybe_goal_handle.has_error()) {
+    RCLCPP_WARN_STREAM(node->get_logger(),
+                       "Goal request was rejected: " << maybe_goal_handle.error());
+    co_return;
+  }
+  RCLCPP_INFO_STREAM(node->get_logger(),
+                     "Goal request was accepted, starting to wait for the result..");
+  const auto &goal_handle = maybe_goal_handle.value();
+  /// Now wait for the result of the action for 20 seconds.
+  auto maybe_result = co_await goal_handle->result(20s);
+  if (maybe_result.has_error()) {
+    RCLCPP_WARN_STREAM(node->get_logger(), "Action failed: " << maybe_result.error());
 
-    if (maybe_goal_handle.has_error()) {
-      RCLCPP_WARN_STREAM(node->get_logger(),
-                         "Goal request was rejected: " << maybe_goal_handle.error());
-      co_return;
-    }
-
-    const auto &goal_handle = maybe_goal_handle.value();
-    /// Now wait for the result of the action for 20 seconds.
-    auto maybe_result = co_await goal_handle->result(20s);
-    if (maybe_result.has_error()) {
-      RCLCPP_WARN_STREAM(node->get_logger(), "Action failed: " << maybe_result.error());
-      co_return;
-    }
+  } else {
     auto const &result = maybe_result.value();
     if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
       RCLCPP_INFO(node->get_logger(), "Fibonacci done. Size: %zu", result.result->sequence.size());
@@ -53,9 +50,13 @@ int main(int argc, char **argv) {
       RCLCPP_WARN(node->get_logger(), "Action finished with code %d",
                   static_cast<int>(result.code));
     }
-    co_return;
-  });
+  }
 
   rclcpp::spin(node);
+}
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  call_action();
   return 0;
 }
