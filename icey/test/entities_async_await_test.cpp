5a56bc90dbd4c57309b3be91659d01c5fa4f9c0e
Ivo Ivanov <ivo.ivanov@tha.de>
2026-02-21 18:14:45 +0100

    Fixed UAF

--- icey/test/entities_async_await_test.cpp
+++ icey/test/entities_async_await_test.cpp
@@ -102,12 +102,12 @@ TEST_F(AsyncAwaitTwoNodeTest, PubSubTest) {
 
 TEST_F(AsyncAwaitTwoNodeTest, PubSubTest2) {
   const auto l = [this]() -> icey::Promise<int> {
-    std::size_t received_cnt{0};
+    auto received_cnt = std::make_shared<std::size_t>(0);
     receiver_->icey()
         .create_subscription<std_msgs::msg::Float32>("/icey_test/sine_signal")
-        .then([&](auto msg) {
-          EXPECT_EQ(received_cnt, std::size_t(msg->data));
-          received_cnt++;
+        .then([received_cnt](auto msg) {
+          EXPECT_EQ(*received_cnt, std::size_t(msg->data));
+          (*received_cnt)++;
         });
 
     auto timer = sender_->icey().create_timer(100ms);
@@ -125,7 +125,7 @@ TEST_F(AsyncAwaitTwoNodeTest, PubSubTest2) {
     /// We do not await till the published message was received.
     /// Since the ACK from DDS is exposed in rclcpp via PublisherBase::wait_for_all_acked, we could
     /// implement a co_await publish(msg, timeout).
-    EXPECT_EQ(received_cnt, 9);
+    EXPECT_EQ(*received_cnt, 9);
     async_completed = true;
     co_return 0;
   };
